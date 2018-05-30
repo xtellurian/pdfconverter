af01e8e37c87a553104a51e7d8951066567acffd
Rian Finnegan <flanagan89@gmail.com>
2018-05-30 13:24:51 +1000

    now you can set the image to deploy

--- scripts/create-aci.sh
+++ scripts/create-aci.sh
@@ -5,16 +5,18 @@ set -e
 # STORAGE_ACCOUNT_NAME=bullclipml
 # ACI_PERS_SHARE_NAME=dev
 # ACI_PERS_LOCATION=southeastasia
+#IMAGE=flanagan89/pdfconverter:latest
 
 # Change these four parameters as needed
 RG=$1
 STORAGE_ACCOUNT_NAME=$2
 SHARE_NAME=$3
 ACI_LOCATION=$4
+IMAGE=$5
 
-if test "$#" -ne 4; then
-    echo "4 arguments required"
-    echo "Usage: $0 <rg-name> <storage-ac-name> <fileshare-name> <location>"
+if test "$#" -ne 5; then
+    echo "5 arguments required"
+    echo "Usage: $0 <rg-name> <storage-ac-name> <fileshare-name> <location> <image>"
     return 1
 fi
 
@@ -28,7 +30,7 @@ echo "ACI Name: $ACI_NAME"
 
 # Export the connection string as an environment variable. The following 'az storage share create' command
 # references this environment variable when creating the Azure file share.
-# export AZURE_STORAGE_CONNECTION_STRING=`az storage account show-connection-string --resource-group $RG --name $STORAGE_ACCOUNT_NAME --output tsv`
+export AZURE_STORAGE_CONNECTION_STRING=`az storage account show-connection-string --resource-group $RG --name $STORAGE_ACCOUNT_NAME --output tsv`
 
 STORAGE_KEY=$(az storage account keys list --resource-group $RG --account-name $STORAGE_ACCOUNT_NAME --query "[0].value" --output tsv)
 
@@ -36,7 +38,7 @@ az container create \
     --resource-group $RG \
     --location $ACI_LOCATION \
     --name $ACI_NAME \
-    --image flanagan89/pdfconverter:v2 \
+    --image $IMAGE \
     --dns-name-label bullclip-pdfconverter-v1 \
     --ports 80 3000 \
     --azure-file-volume-account-name $STORAGE_ACCOUNT_NAME \
