085ffe971ed80b95d559c9c45fc7513c316f48db
Rian Finnegan <flanagan89@gmail.com>
2018-06-22 13:12:58 +1000

    oneshot converter

---
+++ pdfConverter-oneshot/app.js
@@ -0,0 +1,106 @@
+// configure dotenv file.
+require('dotenv').config()
+
+const path = require('path'), fs = require('fs');
+const mv = require('mv');
+var PDFImage = require("pdf-image").PDFImage;
+
+// get variables from environment
+var startDir = process.env.INPUT_DIR;
+var outputDir = process.env.OUTPUT_DIR;
+var processedDir = process.env.PROCESSED_DIR;
+var recurse = process.env.RECURSE;
+var density = process.env.DENSITY;
+var quality = process.env.QUALITY;
+
+// used to make directories when they don't exist
+var makedir = true;
+
+// density
+if(!density || !Number(density)){
+  density = 300; // default to 300
+}
+density = Number(density); // make sure its a number
+
+// quality
+if(!quality || !Number(quality)){
+  quality = 95; // default to 95
+}
+quality = Number(quality); // make sure its a number
+
+
+
+async function forEachInDir(startPath, filter, asyncOperation) {
+  console.log('Starting from dir ' + startPath + '/');
+  if (!fs.existsSync(startPath)) {
+    console.log("no dir ", startPath);
+    return;
+  }
+
+  var files = fs.readdirSync(startPath);
+  for (var i = 0; i < files.length; i++) {
+    var filename = path.join(startPath, files[i]);
+    var stat = fs.lstatSync(filename);
+    if (stat.isDirectory() && recurse) {
+      await forEachInDir(filename, filter); //recurse
+    }
+    else if (filename.indexOf(filter) >= 0) {
+      console.log('-- found: ', filename);
+      // do something here
+      await asyncOperation(filename, outputDir, processedDir);
+    };
+  };
+};
+
+
+// this get's called on everything matched by forEachInDir
+async function convertPdf(pdfPath, outputDir, processedDir) {
+  // highest quality = 100
+  // density ~~ quality, i.e. pixels per inch / DPI
+  // http://www.imagemagick.org/script/command-line-options.php
+  var pdfImage = new PDFImage(pdfPath, {
+    convertOptions: {
+      "-quality": quality,
+      "-density": density
+    }
+  });
+
+  pdfImage.setConvertExtension("jpeg");
+
+  let numberOfPages = await pdfImage.numberOfPages();
+
+  console.log(`${pdfPath} has ${numberOfPages} pages`);
+
+  for (let page = 0; page < numberOfPages; page++) {
+    console.log(`Trying to convert page ${page} of ${pdfPath}`);
+    let imagePath = await pdfImage.convertPage(page);
+    console.log('Created temporary image at ' + imagePath);
+    // move to outputDir
+    var imageFileName = path.basename(imagePath);
+    console.log(`Moving ${imageFileName} to ${outputDir}`);
+    var imageOutputPath = path.join(outputDir, imageFileName);
+    mv(imagePath, imageOutputPath, { mkdirp: makedir }, (err) => {
+      if(err){
+        console.log(`Error moving ${imagePath} to ${imageOutputPath}`);
+        console.log(err);
+      }
+    });
+  }
+
+ // done converting images - move PDF to processed dir
+  console.log(`Success converting ${pdfPath} . Moving to ${processedDir}`);
+  var pdfFileName = path.basename(pdfPath);
+  mv(pdfPath, path.join(processedDir, pdfFileName), {mkdirp: makedir}, function (err) {
+    if(err){
+      console.log(`Error moving ${pdfPath} to processed dir ${processedDir}`)
+      console.log(err);
+    }
+  });
+}
+
+// this is the root operation - for each PDF in directory
+forEachInDir(startDir, '.pdf', convertPdf).then(() => console.log('DONE!')).catch((err) => {
+  console.log('Caught a top level err')
+  console.log(err);
+});
+
---
+++ pdfConverter-oneshot/scripts/deploy-oneshot-aci.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+# RG=bullclip-machine-learning
+# STORAGE_ACCOUNT_NAME=bullclipml
+# ACI_PERS_SHARE_NAME=dev
+# ACI_PERS_LOCATION=southeastasia
+#IMAGE=flanagan89/pdfconverter:latest
+
+# Change these four parameters as needed
+RG=$1
+STORAGE_ACCOUNT_NAME=$2
+SHARE_NAME=$3
+ACI_LOCATION=$4
+IMAGE=$5
+
+if test "$#" -ne 5; then
+    echo "5 arguments required"
+    echo "Usage: $0 <rg-name> <storage-ac-name> <fileshare-name> <location> <image>"
+    return 1
+fi
+
+ACI_NAME=pdfconverteroneshot
+
+echo "Resource Group: $RG"
+echo "Storage Account: $STORAGE_ACCOUNT_NAME"
+echo "File Share Name: $SHARE_NAME"
+echo "ACI Location: $ACI_LOCATION"
+echo "ACI Name: $ACI_NAME"
+
+# where the Azure File volume will be mounted in container
+FILESHARE="/fileshare"
+# these are env variables for the container 
+PROCESSED_DIR="$FILESHARE/batch/Processed"
+OUTPUT_DIR="$FILESHARE/batch/Images"
+INPUT_DIR="$FILESHARE/batch/Unprocessed"
+
+
+
+# Export the connection string as an environment variable. The following 'az storage share create' command
+# references this environment variable when creating the Azure file share.
+export AZURE_STORAGE_CONNECTION_STRING=`az storage account show-connection-string --resource-group $RG --name $STORAGE_ACCOUNT_NAME --output tsv`
+
+STORAGE_KEY=$(az storage account keys list --resource-group $RG --account-name $STORAGE_ACCOUNT_NAME --query "[0].value" --output tsv)
+
+az container create \
+    --resource-group $RG \
+    --location $ACI_LOCATION \
+    --name $ACI_NAME \
+    --image $IMAGE \
+    --restart-policy OnFailure \
+    --azure-file-volume-account-name $STORAGE_ACCOUNT_NAME \
+    --azure-file-volume-account-key $STORAGE_KEY \
+    --azure-file-volume-share-name $SHARE_NAME \
+    --azure-file-volume-mount-path /$FILESHARE/ \
+    --environment-variables INPUT_DIR=$INPUT_DIR OUTPUT_DIR=$OUTPUT_DIR PROCESSED_DIR=$PROCESSED_DIR DENSITY=400 QUALITY=95
